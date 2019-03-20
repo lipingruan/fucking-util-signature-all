d9e28e5aedc526e18d837d7a1dc18d4146d8a3af
Liping Ruan <hello@ruanmail.com>
2019-03-20 16:58:41 +0800

    remove unused codes

--- index.js
+++ index.js
@@ -9,13 +9,3 @@
 
 	module.exports = require ( 'fucking-util-signature' );
 // #endif
-
-// #ifdef APP-PLUS
-
-	module.exports = require ( 'fucking-util-signature' );
-// #endif
-
-// #ifdef APP-PLUS-NVUE
-
-	module.exports = require ( 'fucking-util-signature' );
-// #endif
