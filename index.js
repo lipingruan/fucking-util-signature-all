ccdf71f37df85dde967d66e75265d65c81d4eae6
Liping Ruan <hello@ruanmail.com>
2019-03-20 16:57:19 +0800

    support app

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 
-// #ifdef MP-WEIXIN
+// #ifdef MP
 
 	module.exports = require ( 'fucking-util-signature-uni' );
 // #endif
 
 
-// #ifdef H5
+// #ifdef H5 || APP-PLUS || APP-PLUS-NVUE
+
+	module.exports = require ( 'fucking-util-signature' );
+// #endif
+
+// #ifdef APP-PLUS
+
+	module.exports = require ( 'fucking-util-signature' );
+// #endif
+
+// #ifdef APP-PLUS-NVUE
 
 	module.exports = require ( 'fucking-util-signature' );
 // #endif
