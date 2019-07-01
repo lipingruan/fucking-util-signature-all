579411c80437d9c18af784f213e97a5127a1b4cc
Liping Ruan <hello@ruanmail.com>
2019-07-01 17:28:56 +0800

    修复hbuilder5+加密报错
    https://ask.dcloud.net.cn/article/35851

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 
-// #ifdef MP
+// #ifdef MP || APP-PLUS || APP-PLUS-NVUE
 
 	module.exports = require ( 'fucking-util-signature-uni' );
 // #endif
 
 
-// #ifdef H5 || APP-PLUS || APP-PLUS-NVUE
+// #ifdef H5
 
 	module.exports = require ( 'fucking-util-signature' );
 // #endif
